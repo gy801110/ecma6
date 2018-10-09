8a88462951aecf837e93ba42e31ffcfb0104a9ce
gy801110 <gy801110@naver.com>
2018-10-09 19:08:09 +0900

    ECMA6 Sample.

--- 0102/main.js
+++ 0102/main.js
@@ -1,8 +1,90 @@
+function write(...args) {
+    for (const arg of args) {
+        document.write(arg);
+    }
+    document.write("<br />");
+}
+
+/* Sample1 Example. */
+write("***Sample1***")
 one = 100;
 
 function get() {
     one = 300;
-    console.log("함수:", one);
+    write("Function:", one);
 }
 get();
-console.log("글로벌:", one);
+write("Golbal:", one);
+write("");
+
+/* Sample2 Example. */
+write("***Sample2***")
+let sports = "축구";
+if (sports) {
+    let sports = "농구";
+    write("Block : ", sports);
+}
+write("Golbal : ", sports);
+write("");
+
+/* Sample3 Example. */
+write("***Sample3***")
+var music3 = "음악";
+let sports3 = "축구";
+write("this.music : ", this.music3);
+write("this.sports :", this.sports3);
+write("");
+
+/* Sample4 Example. */
+write("***Sample4***")
+let sports4 = "축구", music4 = "재즈";
+
+function get4() {
+    let music2 = "클래식";
+    write(music4);
+    write(sports4);
+}
+get4();
+write("");
+
+/* Sample5 Example. */
+write("***Sample5***")
+var sports5 = "축구";
+let music5 = "재즈";
+
+function get5() {
+    var sports5 = "농구";
+    let music5 = "클래식";
+
+    write("1: ", sports5);
+    write("2: ", this.sports5);
+    write("3: ", this.music5);
+}
+get5();
+write("");
+
+/* Sample6 Example. */
+write("***Sample6***")
+let sample6 = (one, two) => {
+    return one + two
+};
+let result6 = sample6(1, 2);
+write("result", result6);
+write("");
+
+let sample6_1 = (one, two) => one + two;
+let reulst6_1 = sample6_1(1, 2);
+write(reulst6_1);
+write("")
+
+/* Sample7 Example. */
+write("***Sample7***")
+let get7 = value => value + 10;
+let result7_1 = get7(20);
+write(result7_1);
+write("")
+
+let noParam7 = () => 3 + 4;
+let result7_2 = noParam7();
+write(result7_2);
+write("")
--- README.md
+++ README.md
@@ -3,12 +3,13 @@ ECMA6 &amp; Typescript
 
 
 # Init
-npm install -g webpack
-
-npm install -g webpack-cli
-
 npm install
 
 npm run build
 
 npm run dev
+
+
+# run program
+Change webpack.config.js's entry
+Ex) entry '/0102/main.js' -> '/0103/main.js'
